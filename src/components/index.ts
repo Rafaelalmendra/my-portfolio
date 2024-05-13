7f8ce8c2054b581ca6e80a42081820fbd77be3c8
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-05-13 20:13:09 -0300

    feat: add links in buttons

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,7 @@ export * from './shared/navbar';
 export * from './shared/theme-toggle';
 export * from './shared/theme-provider';
 
+export * from './ui/dialog';
 export * from './ui/button';
+export * from './ui/command';
 export * from './ui/dropdown-menu';
--- src/components/shared/navbar/index.tsx
+++ src/components/shared/navbar/index.tsx
@@ -2,67 +2,82 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-import { motion } from 'framer-motion';
 import { useTheme } from 'next-themes';
 
 import { navbarItems } from 'mocks';
 
 import { Button, ThemeToggle } from 'components';
 
-import { Github, Linkedin, Search } from 'lucide-react';
+import { Github, Linkedin, Menu, Search } from 'lucide-react';
 
 const Navbar = () => {
+  const pathname = usePathname();
   const { theme } = useTheme();
 
   return (
-    <nav className="w-full h-[5.25rem] flex items-center justify-center">
-      <div className="max-w-7xl w-full flex items-center justify-between">
+    <nav className="w-full h-[4.5rem] lg:h-[5.25rem] flex items-center justify-center">
+      <div className="max-w-7xl w-full flex items-center justify-between px-4 lg:px-0">
         <div className="flex items-center gap-7">
-          <motion.div
-            initial={{ opacity: 0, y: -18 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.4, type: 'spring' }}
-          >
-            <Link href="/">
-              <div className="relative w-[29px] h-[34px] hover:translate-y-[-2px] transition-transform">
-                <Image
-                  layout="fill"
-                  className="object-contain"
-                  src={
-                    theme === 'dark'
-                      ? '/logos/logo-simple-white.svg'
-                      : '/logos/logo-simple-black.svg'
-                  }
-                  alt="Logo do Rafael Almendra - Desenvolvedor Front-end"
-                />
-              </div>
-            </Link>
-          </motion.div>
+          <Link href="/">
+            <div className="relative w-[29px] h-[34px] hover:translate-y-[-2px] transition-transform">
+              <Image
+                layout="fill"
+                className="object-contain"
+                src={
+                  theme === 'dark'
+                    ? '/logos/logo-simple-white.svg'
+                    : '/logos/logo-simple-black.svg'
+                }
+                alt="Logo do Rafael Almendra - Desenvolvedor Front-end"
+              />
+            </div>
+          </Link>
 
-          <div className="flex items-center gap-4">
+          <div className="hidden lg:flex items-center gap-4">
             {navbarItems.map((item) => (
               <Link key={item.id} href={item.href}>
-                <Button variant="ghost">{item.title}</Button>
+                <Button variant={pathname === item.href ? 'default' : 'ghost'}>
+                  {item.title}
+                </Button>
               </Link>
             ))}
           </div>
         </div>
 
-        <div className="flex items-center gap-3">
+        <div className="hidden lg:flex items-center gap-3">
           <Button variant="outline" size="icon">
             <Search size={22} />
           </Button>
 
-          <Button variant="outline" size="icon">
-            <Github size={22} />
-          </Button>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Rafaelalmendra"
+          >
+            <Button variant="outline" size="icon">
+              <Github size={22} />
+            </Button>
+          </a>
 
-          <Button variant="outline" size="icon">
-            <Linkedin size={22} />
-          </Button>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/rafaelalmendraa/"
+          >
+            <Button variant="outline" size="icon">
+              <Linkedin size={22} />
+            </Button>
+          </a>
 
           <ThemeToggle />
+        </div>
+
+        <div className="block lg:hidden">
+          <Button variant="outline" size="icon">
+            <Menu size={22} />
+          </Button>
         </div>
       </div>
     </nav>
