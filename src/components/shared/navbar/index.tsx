e905b060f4cc4b8150b77608b1a1bcb55062b616
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-05-13 18:42:44 -0300

    feat: improve logo in dark mode

--- src/components/shared/navbar/index.tsx
+++ src/components/shared/navbar/index.tsx
@@ -1,22 +1,35 @@
+'use client';
+
 import Image from 'next/image';
+import Link from 'next/link';
+
+import { useTheme } from 'next-themes';
 
 import { Button, ThemeToggle } from 'components';
 
 import { Github, Linkedin, Search } from 'lucide-react';
 
 const Navbar = () => {
+  const { theme } = useTheme();
+
   return (
     <nav className="w-full h-[4.625rem] flex items-center justify-center">
       <div className="max-w-7xl w-full flex items-center justify-between">
         <div className="flex items-center gap-7">
-          <div className="relative w-[29px] h-[34px]">
-            <Image
-              layout="fill"
-              className="object-contain"
-              src="/logos/logo-simple-black.svg"
-              alt="Logo do Rafael Almendra - Desenvolvedor Front-end"
-            />
-          </div>
+          <Link href="/">
+            <div className="relative w-[29px] h-[34px]">
+              <Image
+                layout="fill"
+                className="object-contain"
+                src={
+                  theme === 'dark'
+                    ? '/logos/logo-simple-white.svg'
+                    : '/logos/logo-simple-black.svg'
+                }
+                alt="Logo do Rafael Almendra - Desenvolvedor Front-end"
+              />
+            </div>
+          </Link>
         </div>
 
         <div className="flex items-center gap-3">
