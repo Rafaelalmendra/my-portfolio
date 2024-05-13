b4b97a8e47143f7318fe36ec53488354e69c1281
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-05-13 17:59:18 -0300

    refactor: install and configure shadcn

--- src/components/shared/navbar/index.tsx
+++ src/components/shared/navbar/index.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image';
+
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className="w-full h-[4.625rem] flex items-center justify-center">
+      <div className="max-w-7xl w-full flex items-center justify-between">
+        <div className="flex items-center gap-7">
+          <div className="relative w-[29px] h-[34px]">
+            <Image
+              layout="fill"
+              className="object-contain"
+              src="/logos/logo-simple-black.svg"
+              alt="Logo do Rafael Almendra - Desenvolvedor Front-end"
+            />
+          </div>
+        </div>
+
+        <div className="flex items-center gap-3"></div>
+      </div>
+    </nav>
   );
 };
 
