670dbdc47de4c1ea46ebdc4382b36ef519baaf82
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-05-13 18:48:39 -0300

    feat: add links in navbar

--- src/components/shared/navbar/index.tsx
+++ src/components/shared/navbar/index.tsx
@@ -3,7 +3,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { motion } from 'framer-motion';
 import { useTheme } from 'next-themes';
+
+import { navbarItems } from 'mocks';
 
 import { Button, ThemeToggle } from 'components';
 
@@ -13,23 +16,37 @@ const Navbar = () => {
   const { theme } = useTheme();
 
   return (
-    <nav className="w-full h-[4.625rem] flex items-center justify-center">
+    <nav className="w-full h-[5.25rem] flex items-center justify-center">
       <div className="max-w-7xl w-full flex items-center justify-between">
         <div className="flex items-center gap-7">
-          <Link href="/">
-            <div className="relative w-[29px] h-[34px]">
-              <Image
-                layout="fill"
-                className="object-contain"
-                src={
-                  theme === 'dark'
-                    ? '/logos/logo-simple-white.svg'
-                    : '/logos/logo-simple-black.svg'
-                }
-                alt="Logo do Rafael Almendra - Desenvolvedor Front-end"
-              />
-            </div>
-          </Link>
+          <motion.div
+            initial={{ opacity: 0, y: -18 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.4, type: 'spring' }}
+          >
+            <Link href="/">
+              <div className="relative w-[29px] h-[34px] hover:translate-y-[-2px] transition-transform">
+                <Image
+                  layout="fill"
+                  className="object-contain"
+                  src={
+                    theme === 'dark'
+                      ? '/logos/logo-simple-white.svg'
+                      : '/logos/logo-simple-black.svg'
+                  }
+                  alt="Logo do Rafael Almendra - Desenvolvedor Front-end"
+                />
+              </div>
+            </Link>
+          </motion.div>
+
+          <div className="flex items-center gap-4">
+            {navbarItems.map((item) => (
+              <Link key={item.id} href={item.href}>
+                <Button variant="ghost">{item.title}</Button>
+              </Link>
+            ))}
+          </div>
         </div>
 
         <div className="flex items-center gap-3">
---
+++ src/mocks/navbar-items.ts
@@ -0,0 +1,24 @@
+const navbarItems = [
+  {
+    id: 1,
+    title: 'Trabalhos',
+    href: '/works',
+  },
+  {
+    id: 2,
+    title: 'Sobre',
+    href: '/about',
+  },
+  {
+    id: 3,
+    title: 'Artigos',
+    href: '/articles',
+  },
+  {
+    id: 4,
+    title: 'Contato',
+    href: '/contact',
+  },
+];
+
+export { navbarItems };
